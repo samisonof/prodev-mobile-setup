801ba1b517b47d3f79b689450a4c571c2ba76cbd
Samuel <samiberakih3@gmail.com>
2025-08-28 13:10:06 +0300

    Update some changes

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -2,29 +2,17 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Entry Screen - Awesome</Text>
-
-      <View>
-        <Text style={styles.largeText}>
-          Typescript is great if you practice more
-        </Text>
-        <Text style={styles.mediumText}>
-          React Native provides you a single codebase for cross platforms
-        </Text>
-        <Text style={styles.smallText}>ALX is awesome</Text>
-      </View>
-    </View>
+    <View>
+  <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+  <Text style={styles.smallText}>ALX is awesome</Text>
+</View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#90caf9",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
   },
   largeText: {
     fontSize: 30,
